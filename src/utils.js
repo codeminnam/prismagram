3646b80ced0f7133a00511035f9430b094579aea
cineminnam <cine.minnam@gmail.com>
2020-04-20 17:13:11 +0900

    #3.3 sendMail Function with Nodemailer
    #3.3 sendMail Function with Nodemailer

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,24 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({path: path.resolve(__dirname,".env")});
+
 import {adjectives, nouns} from "./words";
+import sgMail from "@sendgrid/mail";
 
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random()*adjectives.length);
     return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 }
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export const sendSecretMail = (address, secret) => {
+    
+    const email = {
+        from: process.env.SENDGRID_USERNAME,
+        to: address,
+        subject: "Login Secret for Prismagram",
+        html: `Hello! Your login secret is ${secret}.<br/> Copy paste on the website to log in`
+    };
+    return sgMail.send(email);
+};
