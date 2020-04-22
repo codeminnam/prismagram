5b6aa2004d80a25cbf04873e78e0bc6c9f903e37
cineminnam <cine.minnam@gmail.com>
2020-04-23 00:05:23 +0900

    #3.16 itsMe and amIFollowing Fileds part Two
    #3.16 itsMe and amIFollowing Fileds part Two

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -5,27 +5,24 @@ export default {
         fullName: parent => {
             return `${parent.firstName} ${parent.lastName}`;
         },
-        amIFollowing: async(parent, _, {request}) => {
+        isFollowing: async(parent, _, {request}) => {
             const {user} = request;
             const {id: parentId} = parent;
             console.log(user.id, parentId);
             try{
-                const exists = await prisma.$exists.user({
-                    AND: [{id: parentId}, {followers_some: [user.id]}]
+                return prisma.$exists.user({
+                    AND: [
+                        {id: parentId}, 
+                        {followers_some: {
+                            id: user.id
+                        }}]
                 });
-                console.log(exists);
-                if(exists){
-                    return true;
-                }else{
-                    console.log(error);
-                    return false;
-                }
             }catch(err){
                 console.log(err);
                 return false;
             }
         },
-        itsMe: (parent, _, {request}) => {
+        isSelf: (parent, _, {request}) => {
             const {user} = request;
             const {id: parentId} = parent;
             return user.id === parentId;
